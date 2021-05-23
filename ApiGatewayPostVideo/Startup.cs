9e55c0b7838dc461b10776a4adcd159f3cbd448e
Mataguiris Palotes <felixwebdev@gmail.com>
2021-05-23 10:35:02 +0200

    Add project files.

---
+++ ApiGatewayPostVideo/Startup.cs
@@ -0,0 +1,92 @@
+using ApiGatewayPostVideo.Data;
+using ApiGatewayPostVideo.Token;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using ProjectBeer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGatewayPostVideo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string cadena = this.Configuration.GetConnectionString("cadenaazure");
+            services.AddTransient<RepositoryBeer>();
+            //services.AddDbContext<BeerContext>(x => x.UseSqlServer(cadena));
+            services.AddDbContextPool<BeerContext>(options => options.UseMySql(cadena, ServerVersion.AutoDetect(cadena)));
+
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc(name: "v1", new OpenApiInfo
+                {
+                    Title = "Api Project Beer",
+                    Version = "v1",
+                    Description = "Api Project Beer"
+                });
+            });
+
+            HelperToken helper = new HelperToken(this.Configuration);
+            //AÑADIMOS AUTENTIFICACION A NUESTRO SERVICIO
+            services.AddAuthentication(helper.GetAuthOptions())
+                .AddJwtBearer(helper.GetJwtOptions());
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Api v1");
+                x.RoutePrefix = "";
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(options => options.AllowAnyOrigin());
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
